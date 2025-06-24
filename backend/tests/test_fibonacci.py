7cfbfd537ec87883d295787103eae901740d5540
zakmahmed <zak2mahmed@gmail.com>
2025-06-24 16:21:23 +0100

    Refactor fibonacci code to accomodate step by step execution

--- backend/tests/test_fibonacci.py
+++ backend/tests/test_fibonacci.py
@@ -1,59 +1,50 @@
 import unittest
-from backend.algorithms.fibonacci import recursive_fibonacci, memo_fibonacci, tab_fibonacci
+from backend.algorithms.fibonacci import fibonacci_recursive_trace, fibonacci_memo_trace, fibonacci_tab_trace
+
+
+FIBONACCI_TEST_CASES = [
+    (0 , 0),
+    (1, 1),
+    (2, 1),
+    (5, 5),
+    (8, 21),
+    (10, 55),
+]
+
 
 class fibonacciTest(unittest.TestCase):
     
     
-    ''' Tests for Fibonacci implementation each test goes over the recursive, memoisation and tabulation
-    versions for the following cases:
-    1. Expected Values
-    2. Base case Value (1)
-    3. Base case Value (2)
-    4. Large Expected Value
-    5. Zero Value
-    6. Negative Value
-    7. Empty Input Value'''
+    ''' Tests for Fibonacci implementation each test goes over the recursive, memoisation and tabulation versions'''
     
-    def test_fibonacci_zero_case(self):
-        with self.assertRaises(ValueError):
-            recursive_fibonacci(0)
-            memo_fibonacci(0)
-            tab_fibonacci(0)
+    def test_fibonacci_tab_zero_case(self):
+        trace = fibonacci_tab_trace(0)
+        self.assertEqual(len(trace), 1)
+        self.assertEqual(trace[0]['type'], 'final_result')
+        self.assertEqual(trace[0]['result'], 0)
         
         
-    def test_fibonacci_base_case_1(self):
-        self.assertEqual(recursive_fibonacci(1), 1)
-        self.assertEqual(memo_fibonacci(1), 1)
-        self.assertEqual(tab_fibonacci(1), 1)
-    
-    def test_fibonacci_base_case_2(self):
-        self.assertEqual(recursive_fibonacci(2), 1)
-        self.assertEqual(memo_fibonacci(2), 1)
-        self.assertEqual(tab_fibonacci(2), 1)
-        
-        
-    def test_fibonacci_negative_case(self):
-        with self.assertRaises(ValueError):
-            self.recursive_return = recursive_fibonacci(-400)
-            self.memo_return = memo_fibonacci(-400)
-            self.tab_return = tab_fibonacci(-400)
-        
-        
-    def test_fibonacci_positive_integer_case(self):
-        self.assertEqual(recursive_fibonacci(5), 5)
-        self.assertEqual(memo_fibonacci(5), 5)
-        self.assertEqual(tab_fibonacci(5), 5)
-        
-    def test_fibonacci_large_positive_integer_case(self):
-       self.assertEqual(recursive_fibonacci(30), 832040)
-       self.assertEqual(memo_fibonacci(30), 832040)
-       self.assertEqual(tab_fibonacci(30), 832040)
+    def test_fibonacci_recursive(self):
+       for n, expected_result in FIBONACCI_TEST_CASES:
+           with self.subTest(n=n):
+               trace = fibonacci_recursive_trace(n)
+               self.assertEqual(trace[-1]['result'], expected_result)
+               self.assertIsInstance(trace, list)
+               self.assertTrue(all(isinstance(item, dict) for item in trace))
        
-    def test_empty_input(self):
-         with self.assertRaises(ValueError):
-            self.recursive_return = recursive_fibonacci(-400)
-            self.memo_return = memo_fibonacci(-400)
-            self.tab_return = tab_fibonacci(-400)
+    def test_fibonacci_memo(self):
+       for n, expected_result in FIBONACCI_TEST_CASES:
+           with self.subTest(n=n):
+               trace = fibonacci_memo_trace(n)
+               self.assertEqual(trace[-1]['result'], expected_result)
+               self.assertIsInstance(trace, list)
+               self.assertTrue(all(isinstance(item, dict) for item in trace))
     
     
-    
+    def test_fibonacci_tabulation(self):
+       for n, expected_result in FIBONACCI_TEST_CASES:
+           with self.subTest(n=n):
+               trace = fibonacci_tab_trace(n)
+               self.assertEqual(trace[-1]['result'], expected_result)
+               self.assertIsInstance(trace, list)
+               self.assertTrue(all(isinstance(item, dict) for item in trace))
